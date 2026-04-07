40b1be1fc5621340e47d832d9bb7c5ea5cc3fba8
Lucas Vidal <luccas.lima12@gmail.com>
2026-04-07 20:37:43 -0300

    Alterando imagens usadas no divider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import '../src/styles/global.css';
 import { About, Aulas, Contact, Footer, Header, Hero, Horarios } from './features/home';
 import Divider from "./components/divider/Divider.jsx";
-import img1 from "../src/assets/images/bg-intro.png"
+import img1 from "../src/assets/images/bg-divider-1.png";
+import img2 from "../src/assets/images/bg-divider-2.png";
 
 function App() {
   return <>
@@ -10,7 +11,7 @@ function App() {
   <Divider image={img1}/>
   <Aulas/>
   <Horarios/>
-  <Divider image={img1}/>
+  <Divider image={img2}/>
   <About/>
   <Contact/>
   <Footer/>
